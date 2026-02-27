621040024769cc23f1fa3a594933fc5f994c0a21
F4txhr <fattttoddd@gmail.com>
2026-02-27 15:51:05 +0700

    Upgrade Kotlin to 1.9.0 and fix dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,18 +32,18 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.8"
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
 dependencies {
-    // Compose BOM - older version for SDK 33
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    // Compose BOM
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48")
